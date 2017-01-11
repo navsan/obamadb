a14f6f5d1d3558f07d96ac2284b3a8aaa3d71be5
cramja <marc.spehlmann@gmail.com>
2017-01-11 09:35:51 -0600

    easy build script for fresh build

---
+++ build.sh
@@ -0,0 +1,13 @@
+# This script should be used for fresh builds.
+# otherwise, cd build && make
+
+git submodule init
+git submodule update
+pushd .
+cd third_party/glog
+git apply ../glog_cmake.patch
+popd
+cd build
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+
